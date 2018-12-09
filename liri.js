28afc15995c0f23890731ae02c24496c815d92ad
Speedway <harpalassi@gmail.com>
2018-12-09 16:05:15 -0600

    more bands edge cases

--- liri.js
+++ liri.js
@@ -48,8 +48,8 @@ function getBands() {
     console.log("\n" + artist + " tour dates:");
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
         function(res) {
-            // console.log(res.data);
-            if (res.data.length === 0) {
+            // console.log(res);
+            if (res.data.length === 0 || res.data.errorMessage || res.data.venue === undefined) {
                 console.log("Sorry, no tour dates currently scheduled.")
             } else if (res.data.length > 0) {
                 for (let i = 0; i < res.data.length; i++) {    
