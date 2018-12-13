fd5cad7a527eb8aec419b000dedda62a79209c73
Speedway <harpalassi@gmail.com>
2018-12-12 20:21:59 -0600

    refactored doWhat()

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-/// MODULES
+//  MODULES
 
 let dotenv = require("dotenv").config();
 let axios = require("axios");
@@ -7,15 +7,14 @@ let keys = require("./keys.js");
 let Spotify = require("node-spotify-api");
 let fs = require('fs');
 
-/// OMDB - searching for movie data
+//  OMDB - searching for movie data
 
-function getFilm() {
-    let query = process.argv.slice(3).join(" ");
+function getFilm(query) {
     if (query == "") {
-        query = "Mr nobody";
+        query = "Mr Nobody";
     }
-    axios.get("https://www.omdbapi.com/?t="+ query +"&apikey=c4a1de54&plot=full").then(
-        function(res) {
+    axios.get("https://www.omdbapi.com/?t=" + query + "&apikey=c4a1de54&plot=full").then(
+        function (res) {
             let movie = res.data;
             if (movie.Title == undefined) {
                 console.log("There was no movie found in our database.")
@@ -28,141 +27,93 @@ function getFilm() {
                 console.log(`Langauges: ${movie.Language}.`)
                 console.log(`The plot is "${movie.Plot}"`)
                 console.log(`The main actors are: ${movie.Actors}.\n`)
-        } 
-    })
+            }
+        })
 }
 
-/// BANDSINTOWN - searching for concert listings
+//  BANDSINTOWN - searching for concert listings
 
-function getBands() {
-    let artist = process.argv.slice(3).join(" ");
+function getBands(artist) {
     console.log("\n" + artist.toUpperCase() + " tour dates:");
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-        function(res) {
+        function (res) {
             // console.log(res);
             if (res.data.length === 0 || res.data.errorMessage || res.data[0].venue === undefined) {
                 console.log("Sorry, no tour dates currently scheduled.")
             } else if (res.data.length > 0) {
-                for (let i = 0; i < res.data.length; i++) {    
+                for (let i = 0; i < res.data.length; i++) {
                     if (res.data[i].venue.region === "") {
                         console.log(`\n${res.data[i].venue.name} - ${res.data[i].venue.city}, ${res.data[i].venue.country} - ${moment(res.data[i].datetime).format("MM/DD/YYYY")}`);
                     } else {
                         console.log(`\n${res.data[i].venue.name} - ${res.data[i].venue.city}, ${res.data[i].venue.region} ${res.data[i].venue.country} - ${moment(res.data[i].datetime).format("MM/DD/YYYY")}`);
                     }
                 }
-                        console.log("\n");
-                }
+                console.log("\n");
             }
-        )
+        }
+    )
 };
 
 
-/// SPOTIFY - searching for song details
+//  SPOTIFY - searching for song details
 
-function getSong() {
-    let song = process.argv.slice(3).join(" ");
+function getSong(song) {
     let spotify = new Spotify(keys.spotify);
     if (song == "") {
         song = "the sign ace of base";
     }
-        spotify
+    spotify
         .search({ type: 'track', query: song, limit: 1 })
-        .then(function(response) {         
+        .then(function (response) {
             for (let i = 0; i < response.tracks.items.length; i++) {
                 let spot = response.tracks.items[i];
-                        console.log(`\nArtist: ${spot.artists[0].name}`);
-                        console.log(`Title: ${spot.name}`);
-                        console.log(`Soundclip: ${spot.external_urls.spotify}`)
-                        console.log(`From the album, "${spot.album.name}."\n`)
-                    }  
+                console.log(`\nArtist: ${spot.artists[0].name}`);
+                console.log(`Title: ${spot.name}`);
+                console.log(`Soundclip: ${spot.external_urls.spotify}`)
+                console.log(`From the album, "${spot.album.name}."\n`)
+            }
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
-    });
+        });
 }
 
-/// READ RANDOM.TXT - grabs data written in .txt file 
+//  READ RANDOM.TXT - grabs data written in .txt file 
 
 function doWhat() {
     let fileName = 'random.txt';
-    fs.readFile(fileName, 'utf8', function(error, data) {
+    fs.readFile(fileName, 'utf8', function (error, data) {
         if (error) {
-            console.log(`lol u messed up, read the error dawg...${error}`)
-        }  
-        let fileSplit = (data.split(","));
-        if (fileSplit[0] === "movie-this") {
-            let query = fileSplit[1];
-            axios.get("https://www.omdbapi.com/?t="+ query +"&apikey=c4a1de54&plot=full").then(
-            function(res) {
-            let movie = res.data;
-            if (movie.Title == undefined) {
-                console.log("There was no movie found in our database.")
-            } else {
-                console.log(`\nThe title is "${movie.Title}".`);
-                console.log(`It came out in ${movie.Year}.`);
-                console.log(`IMDB rated it a ${movie.imdbRating}.`)
-                console.log(`Rotten Tomatoes gives it a rating of ${movie.Ratings[1].Value}.`)
-                console.log(`Country: ${movie.Country}.`)
-                console.log(`Langauges: ${movie.Language}.`)
-                console.log(`The plot is "${movie.Plot}"`)
-                console.log(`The main actors are: ${movie.Actors}.\n`)
-        } 
-    })
-        } else if (fileSplit[0] === "concert-this") {
-            let artist = fileSplit[1];
-            console.log("\n" + artist.toUpperCase() + " tour dates:");
-            axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-                function(res) {
-                    if (res.data.length === 0 || res.data.errorMessage || res.data[0].venue === undefined) {
-                        console.log("Sorry, no tour dates currently scheduled.")
-                    } else if (res.data.length > 0) {
-                        for (let i = 0; i < res.data.length; i++) {    
-                            if (res.data[i].venue.region === "") {
-                                console.log(`\n${res.data[i].venue.name} - ${res.data[i].venue.city}, ${res.data[i].venue.country} - ${moment(res.data[i].datetime).format("MM/DD/YYYY")}`);
-                            } else {
-                                console.log(`\n${res.data[i].venue.name} - ${res.data[i].venue.city}, ${res.data[i].venue.region} ${res.data[i].venue.country} - ${moment(res.data[i].datetime).format("MM/DD/YYYY")}`);
-                            }
-                        }
-                                console.log("\n");
-                        }
-                    }
-                )
-        } else if (fileSplit[0] === "spotify-this-song") {   
-            let spotify = new Spotify(keys.spotify);         
-            let song = fileSplit[1];
-            spotify
-            .search({ type: 'track', query: song, limit: 1 })
-            .then(function(response) {
-                for (let i = 0; i < response.tracks.items.length; i++) {
-                    let spot = response.tracks.items[i];
-                            console.log(`\nArtist: ${spot.artists[0].name}`);
-                            console.log(`Title: ${spot.name}`);
-                            console.log(`Soundclip: ${spot.external_urls.spotify}`)
-                            console.log(`From the album, "${spot.album.name}."\n`)   
-                } 
-            })
-            .catch(function(err) {
-            console.log(err);
-            });
+            console.log(`Error message: ${error}`)
         }
-      })
+        let random = (data.split(","));
+        if (random[0] === "movie-this") {
+            getFilm(random[1]);
+        } else if (random[0] === "concert-this") {
+            getBands(random[1]);
+        } else if (random[0] === "spotify-this-song") {
+            getSong(random[1]);
+        }
+    })
 };
 
-/// SWITCH - to activate app
+//  SWITCH - to activate app
 
 let arg2 = process.argv[2];
+let arg3 = process.argv.slice(3).join(" ");
+
 switch (arg2) {
     case "movie-this":
-        getFilm();
-         break;
+        getFilm(arg3);
+        break;
     case "concert-this":
-        getBands();
+        getBands(arg3);
         break;
     case "spotify-this-song":
-        getSong();
+        getSong(arg3);
         break;
     case "do-what-it-says":
-        doWhat(); 
+        doWhat();
         break;
-    default: console.log("\nNo option selected. \nYou must select from 'movie-this', 'concert-this', and 'spotify-this-song'\n")
+    default: console.log("\nNo option selected. \nYou must select from 'movie-this', 'concert-this', or 'spotify-this-song'\n")
 }
